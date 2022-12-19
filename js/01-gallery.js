5c2d9e8f3e8ff8d20fe9f152e600317bb1d5061e
Evgeniy <gerardcobain1@gmail.com>
2022-12-19 14:51:34 +0200

    Виконання 1 завдання.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
+const imagesMarcup = createGaleryCard(galleryItems);
+imagesContainer.insertAdjacentHTML("beforeend", imagesMarcup);
+
+function createGaleryCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <div class = "gallery__item">
+    <a class = gallery__link" href = "${original}">
+    <img class = "gallery__image"
+    src = "${preview}"
+    data-source = "${original}"
+     alt ="${description}"
+     />
+     </a>
+     </div>
+    `;
+    })
+    .join("");
+}
+imagesContainer.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalImage = evt.target.getAttribute("data-source");
+  const instance = basicLightbox.create(`
+    <img src="${originalImage}" width="800" height="600">
+`);
+  console.log(originalImage);
+
+  instance.show();
+});
